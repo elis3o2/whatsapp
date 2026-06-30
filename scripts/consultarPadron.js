6d41c5c23d3681434acd0491c3dbbc63cab4203a
elis3o2 <eli.feuli37@gmail.com>
2026-06-29 22:50:26 -0300

    consulta-Padron-local

--- scripts/consultarPadron.js
+++ scripts/consultarPadron.js
@@ -1,6 +1,4 @@
 const axios = require("axios");
-const FormData = require("form-data");
-
 
 module.exports = {
     async run(vars, numero) {
@@ -8,17 +6,16 @@ module.exports = {
         try {
 
             const response = await axios.get(
-                "http://192.168.64.40:5014/digitalizacion/",
-                {   
-                    proxy: false,
+                "https://salud1.dyndns.org/api/ciudadanopuco/",
+                {
                     params: {
-                        documento: vars.dni,
+                        dni: vars.dni,
                         sexo: vars.sexo,
-                        api_key: "cpjPsswCpdJHAKbvKKJU3Uus9zQmzZB6hLxCejJ0W3GMKSIjF9ulwhC1jwZfKU4m"
+                        api_key: "b4d9fb57-5033-4fdd-b717-68705df38d35"
                     }
                 }
             );
-            const persona = response.data.sisa_data.ciudadano;
+            persona =  response.data.ciudadano
 
             if (persona.fechaNacimiento) {
                 const d = new Date(persona.fechaNacimiento);
@@ -28,40 +25,6 @@ module.exports = {
                     String(d.getUTCMonth() + 1).padStart(2, "0") + "/" +
                     d.getUTCFullYear();
             }
-
-            if (response.data.banderas.bandera_hc === "error"){
-                console.log("HC ERROR")
-                
-                try {
-                    mensaje = `Existe un error con la historia clínica de la siguiente persona:
-                                dni: ${vars.dni}
-                                nombre: ${persona.nombre}
-                                apellido: ${persona.apellido}`
-                   
-                    const form = new FormData();
-
-                    form.append("from_email", "ssp_dirinformatica@rosario.gov.ar");
-                    form.append("subject", "Error en Historia Clinica");
-                    form.append("body", mensaje);
-                    form.append("to_emails", "efeuli0@rosario.gov.ar");
-                    
-                    const mailresponse = await axios.post(
-                        "http://192.168.64.40:5002/send-email/",
-                        form,
-                        {
-                            headers: form.getHeaders(),
-                            proxy: false
-                        }
-                    );
-                    console.log(mailresponse)
-                } catch (e) {
-                    console.error(e);
-
-                    console.log("response:", e.response);
-                    console.log("status:", e.response?.status);
-                    console.log("data:", e.response?.data);
-                }
-            } 
 
             return {
                 next: "ok",
